f768d8b3ae506acdd586010fc2caec5a25de4459
kimaru355 <emmanuelkimaru355@gmail.com>
2024-02-06 23:24:34 +0300

    count vowels and reverse number

--- countVowels.js
+++ countVowels.js
@@ -1,2 +1,25 @@
 // Question 6: Count Vowels
 // Write a program that counts the number of vowels in a sentence.
+
+function countVowels() {
+  let arguments = process.argv.splice(2);
+
+  const countTheVowels = (sentence) => {
+    console.log(typeof sentence);
+    sentence = sentence.split("");
+    vowels = "aeiouAEIOU";
+    count = 0;
+
+    sentence.forEach((letter) => {
+      if (vowels.includes(letter)) {
+        count++;
+      }
+    });
+    return count;
+  };
+
+  arguments = arguments.join(" ");
+  console.log(countTheVowels(arguments));
+}
+
+countVowels();
--- reverse.js
+++ reverse.js
@@ -5,24 +5,47 @@ function reverse() {
   let arguments = process.argv.slice(2);
 
   const reverseNumber = (integer) => {
+    console.log(integer);
     while (integer % 10 === 0) {
-      integer / 10;
+      integer /= 10;
     }
     integer = ("" + integer).split("");
 
-    let index = integer.length / 2 - 1;
+    let index = Math.floor(integer.length / 2);
     let first = 0;
+    let counter = 0;
+    console.log(integer, index);
     while (index >= 0) {
-      let first = integer.splice(integer.length - index);
+      first = integer[index];
+      counter++;
+      if (counter > 1) {
+        break;
+      }
+      console.log(integer);
+      integer.splice(
+        index,
+        1,
+        integer.splice(
+          integer.length - index === integer.length
+            ? -1
+            : integer.length - index
+        )
+      );
+      console.log(integer);
+      integer.splice(index, 0, first);
       index++;
     }
+    integer = integer.join("");
+    return +integer;
   };
 
   arguments.forEach((integer) => {
-    if (integer < 0) {
-      console.log(reverseNumber(integer * -1) * -1);
+    if (+integer < 0) {
+      console.log(reverseNumber(+integer * -1) * -1);
     } else {
-      console.log(reverseNumber(integer));
+      console.log(reverseNumber(+integer));
     }
   });
 }
+
+reverse();
